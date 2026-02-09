f138e93e8df7703a6fd9662be2b842d70d697af1
binia1 <nakazawaakira1@gmail.com>
2026-02-09 20:50:04 +0900

    asds

---
+++ hyobin_markers.js
@@ -0,0 +1,27 @@
+// 파일명: hyobin_markers.js
+// 기본 마커 데이터 (좌표 보정 전 원본 값)
+
+var defaultLandmarksData = [
+    { name: "효빈광역시청(북구 고송동)", lat: -4919, lng: 18779, color: "#7799CC", type: "normal" },
+    { name: "북구청(북구 중수동)", lat: -5758, lng: 25179, color: "#7799CC", type: "normal" },
+    { name: "서구청(서구 당선동)", lat: -7599, lng: 16791, color: "#00AABB", type: "normal" },
+    { name: "중구청(중구 조유동2가)", lat: -9618, lng: 18087, color: "#BB9955", type: "normal" },
+    { name: "동구청(동구 덕현동)", lat: -10073, lng: 21524, color: "#FF9922", type: "normal" },
+    { "name": "남구청(남구 평당동)", "lat": -14029, "lng": 10020, "color": "#DDBBFF", "type": "normal" },
+    { name: "청엽구청(청엽구 청엽동)", lat: -11565, lng: 22485, color: "#006699", type: "normal" },
+    { name: "창전구청(창전구 창전동)", lat: -14412, lng: 25279, color: "#33AAFF", type: "normal" },
+    { name: "안천구청(안천구 안천동)", lat: -9309, lng: 31997, color: "#AA66DD", type: "normal" },
+    { name: "안천구청 이자출장소(안천구 이자동)", lat: -8958, lng: 35828, color: "#AA66DD", type: "normal" },
+    { name: "탄성군청(탄성군 탄성읍 공리)", lat: -16561, lng: 32507, color: "#BBFF64", type: "normal" },
+    { name: "효빈고등법원(북구 중수동)", lat: -6631, lng: 24774, color: "#7799CC", type: "normal" },
+    { name: "효빈역(동구 사가당동)", lat: -9850, lng: 20693, color: "#FF9922", type: "normal" },
+    { name: "효빈고속버스터미널(북구 해서동)", lat: -7895, lng: 20254, color: "#7799CC", type: "normal" },
+    { name: "효빈대학교(서구 당선동)", lat: -7499, lng: 18228, color: "#00AABB", type: "normal" },
+    { name: "고송교차로역(북구 고송동)(도시철도 2,6호선)", lat: -4751, lng: 17979, color: "#7799CC", type: "normal" },
+    { name: "현대백화점 효빈점(북구 고송동)", lat: -4885, lng: 18379, color: "#7799CC", type: "normal" },
+    { name: "애니메이트 효빈점(북구 고송동)", lat: -4773, lng: 18356, color: "#7799CC", type: "normal" },
+    { name: "엽월대학교 병원(남구 어간동)", lat: -11686, lng: 5609, color: "#DDBBFF", type: "normal" },
+    { name: "HD현대중공업", lat: -14500, lng: 13400, color: "#FF9922", type: "normal" },
+    { name: "효빈 종합운동장(북구 입희동)", lat: -5761, lng: 22160, color: "#7799CC", type: "normal" },
+    { name: "HSCO(북구 생곡동)", lat: -8537, lng: 20362, color: "#7799CC", type: "normal" }
+];
